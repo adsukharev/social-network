34708db4d0f193fa85d0547b91af5ae9525b9a7c
Brandon norrey Bashirian <bbashiri@at-l5.21-school.ru>
2019-07-12 18:18:44 +0300

    old

--- web/app/models.py
+++ web/app/models.py
@@ -1,10 +1,10 @@
-import psycopg2
-
-con = psycopg2.connect('dbname='testdb' user='postgres' host='localhost' password='my_password')
-cur = con.cursor()
-cur.execute("CREATE TABLE test(id serial PRIMARY KEY, name varchar, email varchar)")
-cur.execute("SELECT * FROM test")
-items = cur.fetchall()
-con.commit()
-cur.close()
-con.close()
+# import psycopg2
+#
+# con = psycopg2.connect('dbname='testdb' user='postgres' host='localhost' password='my_password')
+# cur = con.cursor()
+# cur.execute("CREATE TABLE test(id serial PRIMARY KEY, name varchar, email varchar)")
+# # cur.execute("SELECT * FROM test")
+# # items = cur.fetchall()
+# con.commit()
+# cur.close()
+# con.close()
