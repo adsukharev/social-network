cd23c4c04222894ddf6de21500792001015e1249
Andrei Sukharev <mr.andrey.sd@gmail.com>
2019-07-24 19:11:23 +0300

    fixed likes, tags, history in userID

--- backend/app/resources/UserId.py
+++ backend/app/resources/UserId.py
@@ -6,39 +6,31 @@ from .Tags import Tags
 class UserId(UsersCommon):
 
     def get(self, user_id):
-        # sql = """
-        #         SELECT  u.user_id, u.login, ,l.likes
-        #         FROM users u
-        #         LEFT JOIN (
-        #             SELECT to_like_fk, array_agg(from_like_fk) as likes
-        #             FROM likes
-        #             GROUP BY 1
-        #             ) l ON u.user_id = l.to_like_fk
-        #         WHERE user_id = %s
-        #     ;"""
         sql = """
-                 SELECT  u.user_id, u.login, u.email, u.user_name, u.age, u.sex, u.preferences,
-                         u.bio, u.avatar ,l.likes, h.history, t.tags
-                 FROM users u
-                 LEFT JOIN (
-                     SELECT to_like_fk, array_agg(from_like_fk) as likes
-                     FROM likes
-                     GROUP BY 1
-                     ) l ON u.user_id = l.to_like_fk
-                 LEFT JOIN (
-                     SELECT to_history_fk, array_agg(from_history_fk) as history
-                     FROM history
-                     GROUP BY 1
-                     ) h ON u.user_id = h.to_history_fk
-                 LEFT JOIN (
+                SELECT  u.user_id, u.login, l.likes, h.history
+                FROM users u
+                LEFT JOIN (
+                      SELECT likes.to_like_fk, array_agg(u.login) as likes
+                      FROM likes
+                      JOIN  users u ON u.user_id = likes.from_like_fk
+                      GROUP BY 1
+                      ) l ON u.user_id = l.to_like_fk
+                LEFT JOIN (
+                      SELECT history.to_history_fk, array_agg(u.login) as history
+                      FROM history
+                      JOIN  users u ON u.user_id = history.from_history_fk
+                      GROUP BY 1
+                      ) h ON u.user_id = h.to_history_fk
+                LEFT JOIN (
                      SELECT user_id as user_id_fk, array_agg(tag_id) as tags
                      FROM users_tags
                      GROUP BY 1
                      ) t ON u.user_id = t.user_id_fk
-                 WHERE user_id = %s
-             ;"""
+                WHERE u.user_id = %s
+            ;"""
+
         record = (user_id,)
-        user = self.base_get_one(sql, record)
+        user = self.base_get_limited_all(sql, record)
         return user
 
     def put(self, user_id):
