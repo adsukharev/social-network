509bd20fd0bee398f6a1eaa54e7d1dfc8262bd66
Andrei Sukharev <mr.andrey.sd@gmail.com>
2019-09-03 11:44:54 +0300

    fix api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 // import { PORT_MACHINE } from "babel-dotenv";
 
 let HOST_URL = process.env.VUE_APP_HOST_URL;
-HOST_URL = "http://localhost:5000";
+// HOST_URL = "http://localhost:5000";
 export default () => {
     return axios.create({
         withCredentials: true,
